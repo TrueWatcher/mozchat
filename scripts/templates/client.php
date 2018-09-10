66c4b53534a7bdbffbf5dd25dedcdbd7e46ac4dd
TrueWatcher <TrueWatcher@users.noreply.github.com>
2018-09-10 21:33:08 +0300

    1.7.1 improved layout

--- scripts/templates/client.php
+++ scripts/templates/client.php
@@ -4,7 +4,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Multimedia chat</title>
   <link rel="stylesheet" type="text/css" href="assets/<?php print(version($cssLink)); ?>" media="all" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.5" />
 </head>
 <body>
 
